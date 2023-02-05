9ab6b1d814308989752831722ac7031ab79e57e0
Pouria Hemmati <pouria.hemati1369@gmail.com>
2023-02-05 14:50:50 +0330

    added Kotlin-Extension-Functions

---
+++ view/VisibleAndInvisibleView.kt
@@ -0,0 +1,10 @@
+
+import android.view.View
+
+fun View.showInvisible(isShown: Boolean) {
+    if (isShown) {
+        this.visibility = View.VISIBLE
+    } else {
+        this.visibility = View.INVISIBLE
+    }
+}
